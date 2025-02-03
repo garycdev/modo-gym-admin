624fd93d01b2728b9ce9f063e6a768c02428dcd8
garycdev <admin@garycdev.com>
2025-02-03 10:48:50 -0400

    login app

--- app/Http/Controllers/API/LoginAppController.php
+++ app/Http/Controllers/API/LoginAppController.php
@@ -3,6 +3,7 @@ namespace App\Http\Controllers\API;
 
 use App\Http\Controllers\Controller;
 use App\Models\UsuarioLogin;
+use App\Models\Usuarios;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Hash;
