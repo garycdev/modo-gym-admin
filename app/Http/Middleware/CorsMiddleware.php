1df5460ded4bf5ab84989ce9b12e32726d07e3db
garycdev <admin@garycdev.com>
2025-03-11 14:23:42 -0400

    cors

---
+++ app/Http/Middleware/CorsMiddleware.php
@@ -0,0 +1,31 @@
+<?php
+namespace App\Http\Middleware;
+
+use Closure;
+use Illuminate\Http\Request;
+use Symfony\Component\HttpFoundation\Response;
+
+class CorsMiddleware
+{
+    /**
+     * Handle an incoming request.
+     *
+     * @param  \Closure(\Illuminate\Http\Request): (\Symfony\Component\HttpFoundation\Response)  $next
+     */
+    public function handle(Request $request, Closure $next): Response
+    {
+        $response = $next($request);
+
+        // Verifica que la respuesta sea una instancia de Response
+        if (! ($response instanceof Response)) {
+            return $response;
+        }
+
+        // Agrega los headers de CORS
+        $response->headers->set('Access-Control-Allow-Origin', '*');
+        $response->headers->set('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
+        $response->headers->set('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+
+        return $response;
+    }
+}
